39729c4ba34cca80688353d40889fd0c440b5df0
catmando <mitch@catprint.com>
2019-04-08 21:43:57 -0400

    initial commit

--- config/initializers/hyperstack.rb
+++ config/initializers/hyperstack.rb
@@ -3,6 +3,7 @@
 Hyperstack.configuration do |config|
   config.transport = :action_cable
   config.prerendering = :off # or :on
+  config.hotloader_ping = 10 # seconds
   config.cancel_import 'react/react-source-browser' # bring your own React and ReactRouter via Yarn/Webpacker
   config.import 'hyperstack/component/jquery', client_only: true # remove this line if you don't need jquery
   config.import 'hyperstack/hotloader', client_only: true if Rails.env.development?
@@ -20,3 +21,24 @@ module Hyperstack
     )
   end
 end if Rails.env.development?
+
+module ReactiveRecord
+  class Broadcast
+    def self.send_to_server(operation, data)
+      salt = SecureRandom.hex
+      authorization = Hyperstack.authorization(salt, data[:channel], data[:broadcast_id])
+      raise 'no server running' unless Hyperstack::Connection.root_path
+      root_path = Hyperstack::Connection.root_path
+      if root_path =~ /^https:\/\/(\d+)-[a-f0-9\-]+\.[^\/]+\/(.+)\/$/
+        root_path = "http://127.0.0.1:#{$1}/#{$2}"
+      end
+      SendPacket.remote(
+        root_path,
+        data,
+        operation: operation,
+        salt: salt,
+        authorization: authorization
+      ).tap { |p| raise p.error if p.rejected? }
+    end
+  end
+end
